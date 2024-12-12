7ef01c9a084e8623107919e5886389598eb7463d
Riccardo Rossi-Erba <144240575+richir3@users.noreply.github.com>
2024-12-12 15:36:33 +0100

    MODIFY view

--- checker/views.py
+++ checker/views.py
@@ -248,17 +248,9 @@ def send_qr_code_mail(request, attender_id):
 
         # generate qr
         code = attender.code
-        qr = qrcode.QRCode(
-            version=1,
-            error_correction=qrcode.constants.ERROR_CORRECT_L,
-            box_size=10,
-            border=4,
-        )
-        qr.add_data(code)
-        qr.make(fit=True)
 
         # Converti il QR Code in un'immagine PNG
-        img = qr.make_image(fill_color="black", back_color="white")
+        img = qrcode.make(code)
         buffer = BytesIO()
         img.save(buffer, format="PNG")
         buffer.seek(0)
